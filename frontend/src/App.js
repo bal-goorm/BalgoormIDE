fa4c1e57f19570a5469f3d46e1317988f14e2b7e
lhg99 <lee991229@naver.com>
2024-06-14 14:59:40 +0900

    :hammer: [FE] 기타 코드 수정, 패키지 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
 import Login from './user/Login.js'
 import Signup from './user/Signup.js'
-import Chat from './chat/Chat.js';
 import { AuthProvider } from './user/auth/AuthContext.js';
 import ProtectedRoute from './user/auth/ProtectedRoute.js';
 import MyPage from './user/MyPage.js';
+import Admin from './user/admin/Admin.js';
+import Delete from './user/Delete.js';
 
 function App() {
   return (
-    <BrowserRouter>
-      <AuthProvider>
+    <AuthProvider>
+      <BrowserRouter>
         <Routes>
+          <Route path='/' element={<Navigate to='/login' />} />
           <Route path='/login' element={<Login />} />
-          <Route path='/mypage' element={<MyPage />} />
-          <Route path='/signup' element={<Signup />}/>
-          {/* <Route path='/edit' element={<EditPage />}></Route> */}
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/mypage' element={<ProtectedRoute><MyPage /></ProtectedRoute>} />
+          <Route path='/admin' element={<ProtectedRoute><Admin /></ProtectedRoute>}></Route>
+          <Route path='/delete' element={<ProtectedRoute><Delete /></ProtectedRoute>}></Route>
+          {/* <Route path='/edit' element={<ProtectedRoute><EditPage /></ProtectedRoute>}></Route> */}
           {/* <Route path='/chat' element={<Chat />}></Route> */}
         </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
