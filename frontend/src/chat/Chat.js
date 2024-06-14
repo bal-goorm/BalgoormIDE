8693b28c2cc1e76ab34d56a9e6df221018e67e98
lhg99 <lee991229@naver.com>
2024-06-14 15:00:02 +0900

    :hammer: [FE] 시간 남을때 구현 예정

--- frontend/src/chat/Chat.js
+++ frontend/src/chat/Chat.js
@@ -1,33 +1,9 @@
-import React, { useState } from 'react'
-import io from 'socket.io-client';
-import { useEffect } from 'react';
+import React from 'react'
 
-// const socket = io("http://localhost:8080");
+function Chat() {
+  return (
+    <div>Chat</div>
+  )
+}
 
-// function Chat() {
-//     useEffect(() => {
-//         socket.on('connect', () => {
-//           console.log('connected');
-//         });
-      
-//         return () => {
-//           socket.off("connect");
-//           socket.close();
-//         }
-//       }, []);
-
-//     const [message, setMessage] = useState("");
-//     const [content, setContent] = useState([]);
-
-//     const sendMessage = () => {
-//         if(message) {
-//             socket.emit('message', message);
-//         }
-//     }
-    
-//     return (
-//     <div>Chat</div>
-//   )
-//}
-
-// export default Chat
+export default Chat
