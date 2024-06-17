5f4341e81f39602337acfb1fb22ae00e7b50f348
lhg99 <lee991229@naver.com>
2024-06-17 20:39:30 +0900

    :hammer: [FE] protectedRouter 추가예정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
+import { Routes, Route, BrowserRouter, Navigate, Router } from 'react-router-dom';
 import Login from './user/Login.js'
 import Signup from './user/Signup.js'
 import { AuthProvider } from './user/auth/AuthContext.js';
@@ -9,22 +9,21 @@ import ProtectedRoute from './user/auth/ProtectedRoute.js';
 import MyPage from './user/MyPage.js';
 import Admin from './user/admin/Admin.js';
 import Delete from './user/Delete.js';
+import Chat from './chat/Chat.js';
 
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
         <Routes>
           <Route path='/' element={<Navigate to='/login' />} />
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
-          <Route path='/mypage' element={<ProtectedRoute><MyPage /></ProtectedRoute>} />
-          <Route path='/admin' element={<ProtectedRoute><Admin /></ProtectedRoute>}></Route>
-          <Route path='/delete' element={<ProtectedRoute><Delete /></ProtectedRoute>}></Route>
+          <Route path='/mypage' element={<MyPage />} />
+          <Route path='/admin' element={<Admin />}></Route>
+          <Route path='/delete' element={<Delete />}></Route>
           {/* <Route path='/edit' element={<ProtectedRoute><EditPage /></ProtectedRoute>}></Route> */}
-          {/* <Route path='/chat' element={<Chat />}></Route> */}
+          <Route path='/chat' element={<Chat />}></Route>
         </Routes>
-      </BrowserRouter>
     </AuthProvider>
   );
 }
