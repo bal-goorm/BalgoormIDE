cc7cfcb36b0c946f0c45d30b1926a8b12fc9bd6d
승헌이 <seungheon-i@seungheon-iui-MacBookPro.local>
2024-06-17 11:08:24 +0900

    :sparkles: [BE] docker 작업 시작

---
+++ Balgoorm_BackEnd/src/main/java/com/balgoorm/balgoorm_backend/ide/service/IdeService.java
@@ -0,0 +1,9 @@
+package com.balgoorm.balgoorm_backend.ide.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class IdeService {
+}
---
+++ dockerTest/src/main/java/com/test/dockertest/controller/CodeSubmission.java
@@ -0,0 +1,2 @@
+package com.test.dockertest.controller;public class CodeSubmission {
+}
