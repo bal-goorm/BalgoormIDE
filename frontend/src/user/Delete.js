08a01e43b918573e46d51b2a6237454cce220f7a
lhg99 <lee991229@naver.com>
2024-06-17 11:11:46 +0900

    :sparkles: [FE] 회원탈퇴 기능 구현

--- frontend/src/user/Delete.js
+++ frontend/src/user/Delete.js
@@ -79,31 +79,33 @@ function Delete({userId}) {
             <h1 className="mt-2" style={{ color: '#3498db' }}>BalGoorm</h1>
           </div>
 
-          <Row className='justify-content-center'>
-            <Col md={6} className='main-content'>
-              <h1 className='text-center fw-bold'>회원탈퇴</h1>
-              <br />
-              <p>회원 탈퇴하시면 발구름 관련 모든 서비스에 대한 정보가 사라집니다.</p>
-              <p>위 내용을 확인하셨으면, 비밀번호를 입력하고 회원탈퇴 버튼을 눌러주세요.</p>
-              <br />
-              <Form>
-                <Form.Group as={Row} className="mb-3">
-                  <Form.Label column sm={3}>비밀번호</Form.Label>
-                  <Col sm={5}>
-                    <Form.Control 
-                    type="password" 
-                    value={pwd}
-                    onChange={(e) => setPwd(e.target.value)}
-                    placeholder='비밀번호를 입력하세요.' />
-                  </Col>
-                </Form.Group>
+          <div className='border border-2 border-secondary rounded'>
+            <Row className='justify-content-center'>
+              <Col md={6} className='main-content'>
+                <h1 className='text-center fw-bold'>회원탈퇴</h1>
+                <br />
+                <p className='fs-4'>회원 탈퇴하시면 발구름 관련 모든 서비스에 대한 정보가 사라집니다.</p>
+                <p className='fs-4'>위 내용을 확인하셨으면, 비밀번호를 입력하고 회원탈퇴 버튼을 눌러주세요.</p>
+                <br />
+                <Form>
+                  <Form.Group as={Row} className="d-flex mb-3">
+                    <Form.Label className='fw-bolder' column sm={3}>비밀번호</Form.Label>
+                    <Col sm={5}>
+                      <Form.Control 
+                      type="password" 
+                      value={pwd}
+                      onChange={(e) => setPwd(e.target.value)}
+                      placeholder='비밀번호를 입력하세요.' />
+                    </Col>
+                  </Form.Group>
 
-                <Button variant="primary" onClick={handleDelete} className="w-100 mt-4">
-                  회원 탈퇴
-                </Button>
-              </Form>
-            </Col>
-          </Row>
+                  <Button variant="primary" onClick={handleDelete} className="w-100 mt-4">
+                    회원 탈퇴
+                  </Button>
+                </Form>
+              </Col>
+            </Row>
+          </div>
         </Container>
         
 
